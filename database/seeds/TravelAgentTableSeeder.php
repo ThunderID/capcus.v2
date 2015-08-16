c217fdbebd808b61392f3e316af3f161ef39e7a6
erickmo <erick.mo@vortege.com>
2015-08-16 22:41:01 +0700

    do #1

---
+++ database/seeds/TravelAgentTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\Tour;
+
+class TravelAgentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$data = ['Panorama Tour', 'Smailing Tour', 'Dwidaya Tour', 'Kaha Tour', 'Avia Tour', 'Bayu Buana Tour'];
+        //
+	    foreach ($data as $k => $x)
+	    {
+	    	// TOUR
+	    	$travel = new \App\TravelAgent;
+	    	$travel->fill([
+	    			'name'	=> $x,
+	    			'email'	=> 'book@' . strtolower(camel_case(str_replace(' ', '', $x))) . '.com'
+	    		]);
+	    	if (!$travel->save())
+	    	{
+	    		dd($travel->email . '' . $travel->getErrors());
+	    	}
+
+			// TOUR DESTINATION
+			$travel->images()->saveMany([
+				new \App\Image(['name' => 'SmallLogo', 'path' => 'http://localhost:8000/images/logo/' . ($k+1) . '.png', 'title' => $x, 'description' => 'Logo ' . $x ]),
+				new \App\Image(['name' => 'LargeLogo', 'path' => 'http://localhost:8000/images/logo/' . ($k+1) . '.png', 'title' => $x, 'description' => 'Logo ' . $x ])
+			]);
+        }
+    }
+}
