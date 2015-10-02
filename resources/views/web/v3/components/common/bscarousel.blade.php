c934a4cca93f21b7dbd34a0b851d66338e722991
erickmo <erick.mo@vortege.com>
2015-10-02 21:57:43 +0700

    update slider with bs carousel

---
+++ resources/views/web/v3/components/common/bscarousel.blade.php
@@ -0,0 +1,27 @@
+<?php
+	$required_variables = ['carousel_items'];
+	foreach ($required_variables as $x)
+	{
+		if (!array_key_exists($x, get_defined_vars(oid)))
+		{
+			throw new Exception($widget_name . ':' . $x . ": Required", 1);
+		}
+	}
+?>
+
+<div id="slider" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		@foreach ($carousel_items as $k => $i)
+			<li data-target="#slider" data-slide-to="{{$k}}" class="{{ $k == 0 ? 'active' : ""}}"></li>
+		@endforeach
+	</ol>
+	<div class="carousel-inner">
+		@foreach ($carousel_items as $k => $i)
+			<div class="item {{$k == 0 ? 'active' : ''}}">
+				<img src='{{ $item->images->where('name', 'LargeImage')->first()->path }}' class='fullwidth' data-link="{{ $item->link_to }}" style='cursor:pointer'>
+			</div>
+		@endforeach
+	</div>
+	<a class="left carousel-control" href="#slider" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#slider" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
