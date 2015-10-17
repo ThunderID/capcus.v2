ce30183c9cc00a9c0e73b4767999b3639da1e337
Erick Mo <erick.mo@vortege.com>
2015-10-17 08:20:07 +0800

    Fix Tourschedule

--- app/Models/TourSchedule.php
+++ app/Models/TourSchedule.php
@@ -72,10 +72,10 @@ class TourSchedule extends BaseModel
 												$q->where('departure', '<=', $date1)->where('departure_until', '>=', $date1);
 											})
 											->orWhere(function($q) use ($date1, $date2) {
-													$q->where('departure', '>=', $date1)->where('departure_until', '<=', $date2);
+													$q->where('departure', '>', $date1)->where('departure_until', '<=', $date2);
 												})
 											->orWhere(function($q) use ($date1, $date2) {
-													$q->where('departure', '>=', $date1)->where('departure', '<=', $date2)->where('departure_until', '>=', $date2);
+													$q->where('departure', '>', $date1)->where('departure', '<=', $date2)->where('departure_until', '>=', $date2);
 												})
 											->orWhere(function($q) use ($date1, $date2) {
 													$q->where('departure', '<=', $date1)->where('departure_until', '>=', $date2);
